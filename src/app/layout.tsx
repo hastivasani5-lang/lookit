f82bbf38ebc996a5b42905e84135004dadda5482
Jenil Gadhiya <jenilgadhiya@Jenils-MacBook-Air.local>
2026-04-06 15:57:05 +0530

    my changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
+"use client";
 
+import "./globals.css";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
 
   return (
     <html lang="en">

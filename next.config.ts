41484d6354af60b69d5f99f96f1d5c9dcafc30a6
Aeshvi Vaviya <aeshvi@gmail.com>
2026-04-20 09:10:54 +0530

    Auto commit

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,110 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn-wl-assets.classplus.co",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "riseuplabs.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ailleron.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.shutterstock.com",
+      },
+      {
+        protocol: "https",
+        hostname: "academy.internguru.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.weblineindia.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.gettyimages.com",
+      },
+      {
+        protocol: "https",
+        hostname: "the-bteam.transforms.svdcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "newleadershipplaybook.org",
+      },
+      {
+        protocol: "https",
+        hostname: "alanstevens.com.au",
+      },
+      {
+        protocol: "https",
+        hostname: "0.academia-photos.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mentoringher.com",
+      },
+      {
+        protocol: "https",
+        hostname: "swrbot.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.squarespace-cdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+      },
+      {
+        protocol: "https",
+        hostname: "noemamag.imgix.net",
+      },
+      {
+        protocol: "https",
+        hostname: "thumbs.dreamstime.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img-c.udemycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static-media.hotmart.com",
+      },
+      {
+        protocol: "https",
+        hostname: "columncontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
     ],
   },
 };
